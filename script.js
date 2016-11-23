07750c16255df0f9200bde5cff30017b9b1437d9
Ofek Gila <ofek@theofekfoundation.org>
2016-11-23 14:35:26 -0800

    Remove get/set cookie

--- script.js
+++ script.js
@@ -1157,24 +1157,6 @@ getElemId('start-ai').addEventListener('click', function () {
 	playAiMove();
 });
 
-function setCookie(cname, cvalue, exdays) {
-		var d = new Date();
-		d.setTime(d.getTime() + (exdays*24*60*60*1000));
-		var expires = "expires="+d.toUTCString();
-		document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
-function getCookie(cname) {
-		var name = cname + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0; i<ca.length; i++) {
-				var c = ca[i];
-				while (c.charAt(0)==' ') c = c.substring(1);
-				if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
-		}
-		return "";
-}
-
 document.addEventListener('keypress', function (event) {
 	switch (event.which) {
 		case 115: case 83: // s
